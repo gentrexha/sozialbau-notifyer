c88f36526ae205df77e7ab56077a32a89413254b
Florim Hamiti <florim.hamiti@aurickproject.com>
2020-09-10 12:39:49 +0200

    Send messege function via facebook messenger implemented

--- src/get_data.py
+++ src/get_data.py
@@ -10,6 +10,7 @@ from selenium.common.exceptions import NoSuchElementException
 from tqdm import tqdm
 import pandas as pd
 import datetime
+import fbchat
 
 
 def get_player_data(data, text: str):
@@ -25,9 +26,22 @@ def get_player_data(data, text: str):
     return data
 
 
+def sendMessage(message):
+    users = ["Gent Rexha", "Florim Hamiti"]
+    username = os.environ["fb-username"]
+    client = fbchat.Client(username, os.environ["fb-password"])
+    for i in range(2):
+        name = users[i]
+        friends = client.searchForUsers(name)  # return a list of names
+        friend = friends[0]
+        sent = client.sendMessage(message, thread_id=friend.uid)
+        if sent:
+            print("Message sent successfully!")
+
+
 def main():
     logger = logging.getLogger(__name__)
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path='chromedriver_linux64')
     # Accept terms and conditions
     driver.get("https://en.onlinesoccermanager.com/Login?nextUrl=%2FCareer")
     driver.implicitly_wait(10)
@@ -100,6 +114,7 @@ def main():
 
 
 if __name__ == "__main__":
+
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
@@ -111,3 +126,6 @@ if __name__ == "__main__":
     load_dotenv(find_dotenv())
 
     main()
+
+    # For sending messages via facebook messenger
+    # sendMessage("Testing")
