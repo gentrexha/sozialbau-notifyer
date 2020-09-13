a13393169695c3890c08c208687b0b4e75120782
Gent Rexha <gent.rexha@gmail.com>
2020-09-14 00:05:17 +0200

    Updated execution script.

--- run_analyzer.bash
+++ run_analyzer.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /home/pi/Projects/osm-transferlist-analyzer/venv/bin/activate
-python3 src/get_data.py
-python3 src/update_data.py
-python3 src/message.py
+python3 /home/pi/Projects/osm-transferlist-analyzer/src/get_data.py
+python3 /home/pi/Projects/osm-transferlist-analyzer/src/update_data.py
+python3 /home/pi/Projects/osm-transferlist-analyzer/src/message.py
